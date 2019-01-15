2d851bbb74ec213c6da47b0dcda87d6420b75d32
PanchenkoNata <natalitsalucru@gmail.com>
2019-01-16 00:10:06 +0200

    JS 2 set

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,5 @@ let elDiv = document.querySelector('.wrapper');
 let elImg = elDiv.querySelector('img');
 
 setTimeout(function(){
-    elImg.setAttribute('src', './images/VanyaKiller.png');
+    elImg.src = './images/VanyaKiller.png');
 }, 1000);
