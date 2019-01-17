7232576f4e3fa57d9d6dc792327b7708a763027f
PanchenkoNata <natalitsalucru@gmail.com>
2019-01-17 14:14:20 +0200

    JS add other method

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,26 @@
-let elDiv = document.querySelector('.wrapper');
-let elImg = elDiv.querySelector('img');
-
-setTimeout( function() {
-    elImg.src = './images/VanyaKiller.png';
-}, 1000);
-
 let linksArray = [  { url: 'https://www.google.com.ua/?hl=ru', name: 'Google'}, 
                     { url: 'http://ffe.procode.com.ua/', name: 'Procode'}, 
                     { url: 'https://dou.ua/', name: 'DouUa'}, 
                     { url: 'https://developer.mozilla.org/uk/', name: 'MDN'}, ];
 
 let elNav = document.createElement( 'nav' );
-
+let html = '';
 for ( let link of linksArray ) {
-    let elLink = document.createElement( 'a' );
-    elLink.href = link.url;
-    elLink.innerHTML = link.name;
-    elLink.style.fontSize = '20px';
-    elLink.style.padding = '20px';
-    elLink.style.textDecoration = 'none';
-    elNav.appendChild( elLink );
+    let linkHtml = `<a href = '${link.url}'>${link.name}</a>`;
+    html += linkHtml;
 }
+elNav.innerHTML = html;
 console.log(elNav);
+
 let elMenu = document.querySelector('.menu');
 elMenu.appendChild( elNav );
+console.log(elMenu);
+
+let elLink = document.querySelectorAll('.menu nav a');
+for ( let link of elLink ) {
+    link.style.fontSize = '40px';
+    link.style.color = 'green';
+    link.style.padding = '20px';
+    link.style.textDecoration = 'none';
+}
+
